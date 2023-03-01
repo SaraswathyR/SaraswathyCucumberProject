3e044c66faaa2f0fb3b01808de1a529063745ae1
SaraswathyR <117795429+SaraswathyR@users.noreply.github.com>
2023-03-02 00:13:19 +0530

    cucumber error updated

--- src/test/java/stepDefinitions/LoginPageSD.java
+++ src/test/java/stepDefinitions/LoginPageSD.java
@@ -1,5 +1,30 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginPageSD {
+	@Given("user launches OrangeHRM page")
+	public void user_launches_orange_hrm_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("validate user is on login page")
+	public void validate_user_is_on_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("user enters username and password and clicks on login button")
+	public void user_enters_username_and_password_and_clicks_on_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("user able to see home page of OrangeHRM")
+	public void user_able_to_see_home_page_of_orange_hrm() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
 
 }
